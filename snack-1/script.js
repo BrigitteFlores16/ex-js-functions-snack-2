ed5372b5716e6b3c1e3348bcdc0a8b070c49ac37
BrigitteFlores16 <macalupufloresyiyibrigitte@gmail.com>
2025-02-25 22:50:23 +0100

    completato esercizio 1,2,3,4

--- snack-1/script.js
+++ snack-1/script.js
@@ -3,3 +3,16 @@
 //Crea una funzione dichiarativa chiamata somma che accetta due numeri e restituisce la loro somma.
 //Poi, definisci la stessa funzione somma ma come funzione anonima assegnata a una variabile
 //Quindi, riscrivi la funzione somma con la sintassi delle arrow functions.
+
+function somma(num1, num2) {
+  return num1 + num2;
+}
+console.log(somma(7, 5));
+
+const sommaAnonima = function (num1, num2) {
+  return num1 + num2;
+};
+console.log(sommaAnonima(4, 2));
+
+const sommaArrow = (num1, num2) => num1 + num2;
+console.log(sommaArrow(3, 5));
--- snack-3/script.js
+++ snack-3/script.js
@@ -2,3 +2,10 @@
 //Crea una funzione eseguiOperazione
 //Definisci una funzione eseguiOperazione che accetta tre parametri: due numeri e una funzione operatore (callback).
 // La funzione deve eseguire l'operazione fornita sui due numeri.
+
+function eseguiOperazione(param1, param2, param3) {
+  const funzioneparam = param3(param1, param2);
+  console.log(funzioneparam);
+}
+const funzionesomma = (num1, num2) => num1 + num2;
+eseguiOperazione(1, 4, funzionesomma);
